6f575223cf27e475e3333dadb2ca11a33e7b316e
Yevgeny Medvedev <myjetpost@gmail.com>
2024-03-11 02:43:53 +0200

    fix(app): various fixes and improvements

--- app/cards/[id]/page.tsx
+++ app/cards/[id]/page.tsx
@@ -1,15 +1,9 @@
-import SideBar from '@/app/components/SideBar';
 import Statuses from '@/app/components/Statuses';
-
-import styles from './page.module.css';
 
 export default async function CardPage() {
   return (
-    <>
-      <SideBar initialBoards={boards} />
-      <div className={styles.cardsBackground}>
-        <Statuses boards={boards} />
-      </div>
-    </>
+    <div className="cards-wrapper">
+      <Statuses />
+    </div>
   );
 }
