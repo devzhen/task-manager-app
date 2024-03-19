63242dfb430ca1f80f592be82a511b2e602a21a7
Yevgeny Medvedev <myjetpost@gmail.com>
2024-03-19 22:47:38 +0200

    chore(app): add new translations

--- app/dictionaries.ts
+++ app/dictionaries.ts
@@ -1,9 +1,12 @@
 import 'server-only';
 import { LOCALE } from './constants';
+import type { DictionaryType } from './types';
 
 export const dictionaries = {
-  [LOCALE.enUS]: () => import('../dictionaries/en.json').then((module) => module.default),
-  [LOCALE.frFr]: () => import('../dictionaries/fr.json').then((module) => module.default),
+  [LOCALE.enUS]: () =>
+    import('../dictionaries/en.json').then((module) => module.default as DictionaryType),
+  [LOCALE.frFr]: () =>
+    import('../dictionaries/fr.json').then((module) => module.default as DictionaryType),
 } as const;
 
 export const getDictionary = async (locale: string) => {
